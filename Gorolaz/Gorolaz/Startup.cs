e2a5adb7d9deb15e6f626990e547253e73c414b4
grzegorz <grzegorz.pietrzak321@gmail.com>
2017-02-02 00:24:31 +0100

    Let's get started and don't stop

---
+++ Gorolaz/Gorolaz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gorolaz.Startup))]
+namespace Gorolaz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
